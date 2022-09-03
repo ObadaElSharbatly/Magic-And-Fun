7a340b9569898d3fb0b695c7b1e6a43750a03117
unknown <obadamoustafa@gmail.com>
2022-09-03 17:40:53 +0200

    contacts open in new window

--- src/pages/home/contents/contacts/components/ContactMethod.jsx
+++ src/pages/home/contents/contacts/components/ContactMethod.jsx
@@ -5,7 +5,13 @@ function ContactMethod({ contact }) {
   const { img, title, text, href } = contact;
 
   return (
-    <a href={href} className="contact-method" title={title}>
+    <a
+      href={href}
+      className="contact-method"
+      title={title}
+      target="_blank"
+      rel="noreferrer"
+    >
       <img src={img} alt={title} width="50" height="50" />
       <p className="contact-method-text">{text}</p>
     </a>
